95240feb62a2c03e1f64208f692807a6d283a1e9
SarayAnta <s.rodriguezanta@gmail.com>
2025-09-06 19:34:36 +0200

    ejercicio 1

--- Variables_condicionales/ej1.js
+++ Variables_condicionales/ej1.js
@@ -1,1 +1,6 @@
-// Primeros ejercicios
+// Ejercicio 1: presentación
+const nombre = "Saray";
+let edad = 30;
+const ciudad = "Móstoles";
+
+console.log(`Hola, me llamo ${nombre}, tengo ${edad} años y vivo en ${ciudad}.`);
