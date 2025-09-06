57619602ec23c9c7efd973b09bb6db56e5f5fe76
SarayAnta <s.rodriguezanta@gmail.com>
2025-09-06 19:46:28 +0200

    ejercicio 2

---
+++ Variables_condicionales/ej2.js
@@ -0,0 +1,11 @@
+// Condicionales
+
+let temperatura = 24;
+
+if (temperatura >= 25) {
+    console.log("Uy maja, qué calor");
+} else if(temperatura >=17 && temperatura <25) {
+    console.log("Con esta temperatura Mariaster no tiene de qué quejarse");
+} else {
+    console.log("Qué frío maja!!");
+}
