3ffb704123d2080448c0a54974f3ef37ce9d11c0
SarayAnta <s.rodriguezanta@gmail.com>
2025-09-08 14:36:41 +0200

    Modificado ejercicio 1

--- funciones_avanz/ej1.js
+++ funciones_avanz/ej1.js
@@ -3,12 +3,14 @@
 // Bonus: que devuelva también la cantidad de pares encontrados.
 
 let arrayEntero = [1, 2, 3, 54, 79, 86, 44, 35, 24, 90, 153, 998];
-let arrayNuevo = [];
-let cantidad = 0;
 
-for (let i = 0; i < arrayEntero.length; i ++) {
-    if (arrayEntero[i] % 2 == 0) {
-        arrayNuevo.push(arrayEntero[i]);
+
+function filtrarPares(array) {
+    let arrayNuevo = [];
+    let cantidad = 0;
+    for (let i = 0; i < array.length; i ++) {
+    if (array[i] % 2 == 0) {
+        arrayNuevo.push(array[i]);
         cantidad ++
     }
 }
@@ -17,3 +19,6 @@ console.log("El número de pares encontrados es de " + cantidad);
 for(let i = 0; i < arrayNuevo.length; i++) {
     console.log(arrayNuevo[i]);
 }
+}
+
+filtrarPares(arrayEntero);
