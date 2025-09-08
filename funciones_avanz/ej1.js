7bdb37a42753072bba57c13dd2eaa783975f4d98
SarayAnta <s.rodriguezanta@gmail.com>
2025-09-08 14:34:21 +0200

    ejercicio 1

--- funciones_avanz/ej1.js
+++ funciones_avanz/ej1.js
@@ -1,1 +1,19 @@
 // Funciones avanzadas
+// Crea una función filtrarPares que reciba un array de números y devuelva un nuevo array solo con los números pares.
+// Bonus: que devuelva también la cantidad de pares encontrados.
+
+let arrayEntero = [1, 2, 3, 54, 79, 86, 44, 35, 24, 90, 153, 998];
+let arrayNuevo = [];
+let cantidad = 0;
+
+for (let i = 0; i < arrayEntero.length; i ++) {
+    if (arrayEntero[i] % 2 == 0) {
+        arrayNuevo.push(arrayEntero[i]);
+        cantidad ++
+    }
+}
+console.log("El número de pares encontrados es de " + cantidad);
+
+for(let i = 0; i < arrayNuevo.length; i++) {
+    console.log(arrayNuevo[i]);
+}
