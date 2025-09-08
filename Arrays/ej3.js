76fe9c3054abf485f716ee5562df2ada581d821d
SarayAnta <s.rodriguezanta@gmail.com>
2025-09-08 14:19:17 +0200

    ejercicio 3

---
+++ Arrays/ej3.js
@@ -0,0 +1,10 @@
+//Objetos básicos
+
+let persona = {
+    nombre: "Saray",
+    edad: 30,
+    ciudad: "Móstoles",
+    hobbies: ["leer", "jugar a rugby", "ver pelis"]
+}
+
+console.log(`Hola, me llamo ${persona.nombre}, tengo ${persona.edad} y me gusta ${persona.hobbies[0]}.`)
